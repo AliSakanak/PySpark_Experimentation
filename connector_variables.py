fe936adede9946053e02fe0812e124fc62b98cdd
AliSakanak <102181822+AliSakanak@users.noreply.github.com>
2022-07-24 11:20:32 +0100

    Create connector_variables.py

---
+++ connector_variables.py
@@ -0,0 +1,9 @@
+MYSQL_JAR = "PySpark_Experimentation/mysql-connector-java-8.0.29.jar"
+MYSQL_URL = "jdbc:mysql://localhost:3306/db_for_spark"
+MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver"
+
+DB_NAME = "db_for_spark"
+DB_USER = "root"
+DB_HOST = "localhost"
+DB_PASSWORD = "kelebek18"
+DB_PORT = 3306
